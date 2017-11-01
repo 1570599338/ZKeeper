ad0ed58526fc0b82cade1e8b273462add5777d1d
1570599338@qq.com <1570599338@qq.com>
2017-11-01 23:33:39 +0800

    修改

--- src/com/lquan/zkeeper/base/ZKBase.java
+++ src/com/lquan/zkeeper/base/ZKBase.java
@@ -82,7 +82,9 @@ public class ZKBase {
 //				   Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT); 
 //		System.out.println(new String(zk.getData("/testRootPath/testChildPathTwo",true,null))); 
 		
-		//zk.delete("/lquan/lquan5", -1);
+		zk.delete("/lquan/lquan5", -1);
+		
+		
 		
 		zk.close();
 		
