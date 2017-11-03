ce99e569f3ee9329560a3fe2f4885abbdf4e7ed7
1570599338@qq.com <1570599338@qq.com>
2017-11-04 01:03:39 +0800

    zookeeper的原生API

---
+++ src/com/lquan/zookeeper/cluster/Client2.java
@@ -0,0 +1,12 @@
+package com.lquan.zookeeper.cluster;
+
+
+
+public class Client2 {
+
+	public static void main(String[] args) throws Exception{
+		
+		ZKWatcher myWatcher = new ZKWatcher();
+		Thread.sleep(100000000);
+	}
+}
